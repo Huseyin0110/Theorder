6cfea91210d370a190891f460fe296dee424a2e6
xecutioner0110 <44304004+xecutioner0110@users.noreply.github.com>
2018-11-02 23:52:57 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -88,7 +88,7 @@ client.on('error', e => {
   console.log(chalk.bgRed(e.replace(regToken, 'that was redacted')));
 });
 
-client.login(ayarlar.token);
+client.login(process.env.BOT_TOKEN);
 
 client.on("message", async message => {
   var user = message.mentions.users.first() || message.author;
